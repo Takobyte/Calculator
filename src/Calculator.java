e157d7c5d8390b57db794020025d1dac688049d7
John So <jungwookso@gmail.com>
2019-12-13 20:38:25 -0800

    calculator project

---
+++ src/Calculator.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+
+public class Calculator {
+    private JTextField resultField;
+    private JButton percentBtn;
+    private JButton clearEntryBtn;
+    private JButton clearBtn;
+    private JButton backBtn;
+    private JButton invertButton;
+    private JButton squareButton;
+    private JButton rootButton;
+    private JButton divideBtn;
+    private JButton sevenBtn;
+    private JButton eightBtn;
+    private JButton nineBtn;
+    private JButton multiplyButton;
+    private JButton fourBtn;
+    private JButton fiveBtn;
+    private JButton sixBtn;
+    private JButton minusBtn;
+    private JButton oneBtn;
+    private JButton twoBtn;
+    private JButton threeBtn;
+    private JButton plusBtn;
+    private JButton equalBtn;
+    private JButton decimalBtn;
+    private JButton zeroBtn;
+    private JButton changeSignBtn;
+    private JPanel calculatorPanel;
+
+
+}

aafe176f12df4133f5c46ccc60982a107f67d8ac
John So <jungwookso@gmail.com>
2019-12-14 01:38:56 -0800

    more test cases

--- src/TestOperation.java
+++ src/TestOperation.java
@@ -36,4 +36,36 @@ public class TestOperation {
         assertEquals(expected,result);
     }
 
+    // Test addition for decimal value .0 ex. 11 =/= 11.0
+    @Test
+    public void testAdditionDecimalNotEql() {
+        BigDecimal result = testAddition.operation("5.0", "6.0");
+        BigDecimal expected = new BigDecimal("11");
+        assertNotEquals(expected,result);
+    }
+
+    // Test negative value addition
+    @Test
+    public void testAdditionNegVal() {
+        BigDecimal result = testAddition.operation("-5.0", "6.0");
+        BigDecimal expected = new BigDecimal("1.0");
+        assertEquals(expected,result);
+    }
+
+    // Test negative result addition
+    @Test
+    public void testAdditionNegResult() {
+        BigDecimal result = testAddition.operation("5.0", "-6.0");
+        BigDecimal expected = new BigDecimal("-1.0");
+        assertEquals(expected,result);
+    }
+
+    // Test 0 value addition
+    @Test
+    public void testAdditionZeroVal() {
+        BigDecimal result = testAddition.operation("5.0", "-5.0");
+        BigDecimal expected = new BigDecimal("0.0");
+        assertEquals(expected,result);
+    }
+
 }
